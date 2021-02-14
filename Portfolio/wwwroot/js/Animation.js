71a009cbd89ae815e3f9f4b49ddc5344de3e8218
shenodasamy <79038225+shenodasamy@users.noreply.github.com>
2021-02-14 11:42:18 -0800

    .

---
+++ Portfolio/wwwroot/js/Animation.js
@@ -0,0 +1,13 @@
+﻿/*window.onload = callRepeater;
+
+function callRepeater() {
+    let element = document.getElementById('test');
+    repeat(200, 0, 100, 1, element);
+}
+
+function repeat(time, start, end, step, element) {
+    for (let i = start; i < end; i++) {
+        element.innerHTML = i;
+        sleep(time);
+    }
+}*/
